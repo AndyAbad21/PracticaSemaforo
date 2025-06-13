6003e62739074767c844a1849b82aa916a6d013a
AndyDev <andyyariana2017@gmail.com>
2025-06-12 20:48:59 -0500

    feat(Interfaz grafica mejoradad): Implementacion de una interfaz grafica mejorada

--- controladorTrafico.py
+++ controladorTrafico.py
@@ -1,4 +1,6 @@
 import multiprocessing
+import os
+import threading
 import time
 
 from semaforo import Semaforo  # Importacion del script del semaforo
@@ -9,6 +11,8 @@ class ControladorTrafico:
 
         # Cola para el controlador de los semaforos
         self.done_queue = multiprocessing.Queue()
+        # Cola para el controlador del color de los semaforos
+        self.estado_queue = multiprocessing.Queue()
 
         # Eventos individuales para activar semáforos
         self.events = {
@@ -25,44 +29,111 @@ class ControladorTrafico:
         # Procesos de semáforos con la barrera del grupo correspondiente (N-S)(E-O)
         self.semaforos = {
             "Norte": Semaforo(
-                "Semaforo Norte", self.events["Norte"], self.done_queue, self.barrera_NS
+                "Norte",
+                self.events["Norte"],
+                self.done_queue,
+                self.estado_queue,
+                self.barrera_NS,
             ),
             "Sur": Semaforo(
-                "Semaforo Sur", self.events["Sur"], self.done_queue, self.barrera_NS
+                "Sur",
+                self.events["Sur"],
+                self.done_queue,
+                self.estado_queue,
+                self.barrera_NS,
             ),
             "Este": Semaforo(
-                "Semaforo Este", self.events["Este"], self.done_queue, self.barrera_EO
+                "Este",
+                self.events["Este"],
+                self.done_queue,
+                self.estado_queue,
+                self.barrera_EO,
             ),
             "Oeste": Semaforo(
-                "Semaforo Oeste", self.events["Oeste"], self.done_queue, self.barrera_EO
+                "Oeste",
+                self.events["Oeste"],
+                self.done_queue,
+                self.estado_queue,
+                self.barrera_EO,
             ),
         }
 
         self.grupos = [["Norte", "Sur"], ["Este", "Oeste"]]
 
+        # Estado visual de cada semáforo
+        self.estado_semaforos = {
+            "Norte": "rojo",
+            "Sur": "rojo",
+            "Este": "rojo",
+            "Oeste": "rojo",
+        }
+
     def iniciar_simulacion(self, ciclos=3):
         for s in self.semaforos.values():
             s.start()
 
-        print("\n--- Iniciando simulación de tráfico sincronizado por grupos ---\n")
+        # print("\n--- Iniciando simulación de tráfico sincronizado por grupos ---\n")
+
+        # Hilo para escuchar estados
+        threading.Thread(target=self.actualizar_estado_visual, daemon=True).start()
 
         for i in range(ciclos):
-            print(f"\n>>> CICLO {i+1}")
+            # print(f"\n>>> CICLO {i+1}")
 
             for grupo in self.grupos:
-                print(f"[CONTROLADOR] Activando grupo: {grupo}")
+                # print(f"[CONTROLADOR] Activando grupo: {grupo}")
                 for direccion in grupo:
                     self.events[direccion].set()
 
                 # Esperar que ambos miembros del grupo terminen su ciclo
                 for _ in grupo:
                     mensaje = self.done_queue.get()
-                    print(f"[CONTROLADOR] Recibido: {mensaje}")
+                    # print(f"[CONTROLADOR] Recibido: {mensaje}")
 
-                time.sleep(1) # Simulacion de tiempo entre cambio de color entre semaforos contrarios
-
+                # Simulacion de tiempo entre cambio de color entre semaforos contrarios
+                time.sleep(1)
         print("\n--- Simulación finalizada ---\n")
         self.detener()
+
+    # --------------- Salida de la consola mejorada ---------------------
+    def actualizar_estado_visual(self):
+        while True:
+            try:
+                nombre, color = self.estado_queue.get(timeout=1)
+                self.estado_semaforos[nombre] = color
+                self.mostrar_tablero()
+            except:
+                continue
+
+    def mostrar_tablero(self):
+        os.system("cls" if os.name == "nt" else "clear")
+
+        def pintar_lineas(nombre):
+            color_actual = self.estado_semaforos[nombre]
+            return [
+                f"{nombre:^20}",
+                f"ROJO     [{'O' if color_actual == 'rojo' else ' '}]",
+                f"AMARILLO [{'O' if color_actual == 'amarillo' else ' '}]",
+                f"VERDE    [{'O' if color_actual == 'verde' else ' '}]",
+            ]
+
+        norte = pintar_lineas("Norte")
+        sur = pintar_lineas("Sur")
+        este = pintar_lineas("Este")
+        oeste = pintar_lineas("Oeste")
+
+        print()
+        for linea in norte:
+            print(linea.center(60))
+
+        print()
+        for l_izq, l_der in zip(oeste, este):
+            print(f"{l_izq:<30} {l_der:>30}")
+
+        print()
+        for linea in sur:
+            print(linea.center(60))
+    # ------------------------------------------------------------------------
 
     def detener(self):
         for s in self.semaforos.values():
--- semaforo.py
+++ semaforo.py
@@ -2,31 +2,37 @@ import multiprocessing
 import time
 
 class Semaforo(multiprocessing.Process):
-    def __init__(self, name, green_event, done_queue, barrier, green_duration=3, yellow_duration=1):
+    def __init__(self, name, green_event, done_queue, estado_queue, barrier, green_duration=3, yellow_duration=1):
         super().__init__(name=name)
         self.green_event = green_event # El que permite iniciar el evento desde el controlador
         self.done_queue = done_queue # Cola para notificar al controlador que el semaforo termino su ciclo
+        self.estado_queue = estado_queue # Cola para notificar al controlador el color actual de semaforo 
         self.barrier = barrier # Barrera para que semaforos contrarios se esperen para notificar
         self.green_duration = green_duration # Tiempo de encendido del color verde
         self.yellow_duration = yellow_duration # Tiempo de encendido del color amarillo
 
     def run(self):
         while True:
-            print(f"[{self.name}] Esperando permiso para verde...")
+            # print(f"[{self.name}] Esperando permiso para verde...")
+            self.estado_queue.put((self.name, "rojo"))
+
+            # Espera que el controlador le active el evento
             self.green_event.wait()
 
             self._cambiar_color("verde", self.green_duration)
             self._cambiar_color("amarillo", self.yellow_duration)
-            self._cambiar_color("rojo")
+            self._cambiar_color("rojo", 0.5)
 
-            print(f"[{self.name}] Esperando en la barrera...")
+            # print(f"[{self.name}] Esperando en la barrera...")
             self.barrier.wait()  # Espera que el semaforo contrario termine (N-S)(E-O)
 
             self.done_queue.put(f"{self.name} terminó su ciclo")
+            
             self.green_event.clear() # Limpiar todo para que el proceso no siga en ejecucion
 
     def _cambiar_color(self, color, duracion=None):
-        print(f"[{self.name}] Cambiando a {color.upper()}")
+        # print(f"[{self.name}] Cambiando a {color.upper()}")
+        self.estado_queue.put((self.name, color))
         if duracion:
             time.sleep(duracion)
         # else:
